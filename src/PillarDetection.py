0d9951e806a7dd92847689a5a9bd91adc84b6f14
B4lint0413 <69457665+B4lint0413@users.noreply.github.com>
2025-05-20 21:54:34 +0200

    Get the positions and sizes of the green pillars

--- src/PillarDetection.py
+++ src/PillarDetection.py
@@ -13,7 +13,16 @@ def MakeImg():
     cam.stop()
     return img
 
-img = cv2.cvtColor(MakeImg(), cv2.COLOR_RGB2HSV)
+# The coordinates of the pillar's point to the left (as we bypass on the left)
+def LeftEnd(points):
+    leftmost = points[0]
+    for point in points:
+        if point[0][0] < leftmost[0][0]:
+            leftmost = point
+    return leftmost[0]
+
+img = cv2.cvtColor(MakeImg, cv2.COLOR_RGB2HSV)
+
 '''
 cv2.imshow("asd", img)
 cv2.waitKey(0)
@@ -25,23 +34,26 @@ print(img.shape)
 lwrGreen = np.array([35,100,50])
 upprGreen = np.array([90,255,255])
 '''
-lwrGreen = np.array([40,50,50])
-lwrGreen = np.array([40,50,50])
-lwrGreen = np.array([40,50,50])
-lwrGreen = np.array([40,50,50])
+lwrRed = np.array([40,50,50])
+upprRed = np.array([40,50,50])
 '''
+#green_masked = cv2.bitwise_and(img,img, mask=green_mask)
+
+# Mask the image and find contours
 green_mask = cv2.inRange(img, lwrGreen, upprGreen)
-green_masked = cv2.bitwise_and(img,img, mask=green_mask)
-'''
 green_contour, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Identify the pillars from the contours
+pillars = []
 for cnt in green_contour:
-    if cv2.contourArea(cnt) > 500:
-        approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
-        if len(approx) == 4:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(img, (x-w, y), (x, y + h), (0, 255, 0), 3)
+    area = cv2.contourArea(cnt)
+    if area > 1500:
+        approx = cv2.approxPolyDP(cnt, 0.05 * cv2.arcLength(cnt, True), True)
         cv2.polylines(img, [approx], True, (0,0,255), 3)
-   '''         
-cv2.imshow("asd", cv2.cvtColor(green_masked, cv2.COLOR_HSV2RGB))
+        pillars.append({"point": LeftEnd(approx), "area": area})
+
+# cv2.imshow("asd", cv2.cvtColor(green_masked, cv2.COLOR_HSV2RGB))
+print("Pillars detected: ", pillars)
+cv2.imshow("Green pilars detected", cv2.cvtColor(img, cv2.COLOR_HSV2RGB))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
