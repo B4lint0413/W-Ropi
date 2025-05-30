2afc00d36b8dad01f9934b590ec648bcd532949d
B4lint0413 <balu050413@gmail.com>
2025-05-30 15:19:46 +0200

    Turning works properly for the left

--- src/DriveController.py
+++ src/DriveController.py
@@ -1,6 +1,5 @@
 from buildhat import Motor
 from WallDistance import GetWallError, IsTurning
-import time
 import math
 
 steering = Motor('D')
@@ -8,50 +7,57 @@ drive = Motor('C')
 wheel_rotation_distance = 17.6 #distance traveled by one rotation of the wheel
 def getOrientation(previous, angle, distance):
     if angle == 0: # prevent zero division
-        return 0
+        return previous
     
     steering_rad = math.radians(angle)
     wheelbase = 18.4 # space between the axles in cm
     turning_rad = wheelbase/math.tan(steering_rad)
     orientation = math.degrees(distance/turning_rad)
     current = previous + orientation
-    if current > 90: # cos function changes sign above 90 and below -90
+    if current > 50: # cos function changes sign above 90 and below -90
         return current-90
-    elif current < -90:
+    elif current < -50:
         return current+90
     return current
-'''
-steering.run_to_position(30)
-previous_orientation = 0
-for i in range(3):
-    pos = drive.get_position()
-    drive.run_for_seconds(1)
-    traveled = (drive.get_position() - pos)/360*wheel_rotation_distance
-    orientation = getOrientation(previous_orientation, 30, traveled)
-    print(orientation)
-    time.sleep(5)
-    previous_orientation = orientation
-'''
-pid = {"wall": {"p": 3, "i": 0.01, "d": 4.3}}
-drive.start(40)
-errorSum = 0
-lastError = 0
-start = time.time()
-drive_rotation = drive.get_position()
-previous_orientation = 0
-degree = 0
-steering.run_to_position(0,100)
-while time.time()-start<6:
-    distance = (drive.get_position()-drive_rotation)/360*wheel_rotation_distance
+try:
+    pid = {"wall": {"p": 3, "i": 0.01, "d": 4.3}}
+    drive.run_for_seconds(18, speed=30, blocking=False)
+    errorSum = 0
+    lastError = 0
     drive_rotation = drive.get_position()
-    orientation = getOrientation(previous_orientation, steering.get_aposition(), distance)
-    error = GetWallError()*13*math.cos(math.radians(orientation)) # multiply by cos to get the actual distance from wall
-    orientation_error = -orientation*0.5 # prevent perpendicularity with the wall 
-    degree = error*pid["wall"]["p"]+errorSum*pid["wall"]["i"]+(error-lastError)*pid["wall"]["d"]+orientation_error
-    print(degree)
-    steering.run_to_position(degree, speed=100, blocking=True)
-    errorSum += error
-    lastError = error
-    previous_orientation = orientation
-drive.stop()
-steering.stop()
+    previous_orientation = 0
+    orientation = 0
+    degree = 0
+    still_turning = 0
+    steering.run_to_position(0,100)
+    while drive.get_speed()>10:
+        try:
+            distance = (drive.get_position()-drive_rotation)/360*wheel_rotation_distance
+            drive_rotation = drive.get_position()
+            '''
+            if IsTurning() == -1:
+                still_turning += 1
+                if still_turning == 3:
+                    orientation += 90
+            elif IsTurning() == 42:
+                still_turning = 0
+            '''
+            orientation = getOrientation(previous_orientation, steering.get_aposition(), distance)
+            print(orientation)
+            error = GetWallError()*16*math.cos(math.radians(orientation)) # multiply by cos to get the actual distance from wall
+            orientation_error = -orientation*0.5#**3*(0.5*(10**-4)) # prevent perpendicularity with the wall 
+            degree = error*pid["wall"]["p"]+errorSum*pid["wall"]["i"]+(error-lastError)*pid["wall"]["d"]+orientation_error
+            degree = 90 if degree>90 else degree
+            degree = -90 if degree<-90 else degree
+            steering.run_to_position(degree, speed=100, blocking=True)
+            errorSum += error
+            lastError = error
+            previous_orientation = orientation
+        except:
+            continue
+    drive.stop()
+    steering.stop()
+except Exception as e:
+    print(e)
+    drive.stop()
+    steering.stop()
--- src/WallDistance.py
+++ src/WallDistance.py
@@ -6,12 +6,18 @@ right = DistanceSensor('B', threshold_distance=200)
 def GetWallError():
     ldist = left.get_distance()
     rdist = right.get_distance()
+    ldist = 2000 if ldist == -1 else ldist
+    rdist = 2000 if rdist == -1 else rdist
     return (rdist-ldist)/900
 
 def IsTurning():
+    ldist = left.get_distance()
+    rdist = right.get_distance()
     if ldist == -1 and rdist != -1:
         return -1
     elif rdist == -1 and ldist != -1:
         return 1
+    elif rdist != -1 and ldist != -1:
+        return 42
     return 0
     
